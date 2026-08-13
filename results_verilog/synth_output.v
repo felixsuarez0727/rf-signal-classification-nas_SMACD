374ef8241872cec8fd76385ed8b5b093c1319f68
felixsuarez0727 <felixdavidsuarezbonilla@gmail.com>
2026-08-12 22:49:04 -0500

    feat: add synthesized netlists via Git LFS (generic + Xilinx target)

---
+++ results_verilog/synth_output.v
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ea32bb27b4a20d8c7303ffd28e814091af9113cacc668e84d7e79d5a2b7e980
+size 105429524
---
+++ results_verilog/synth_xilinx_output.v
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e158ab591387404cb212f6f91f281ca75e575eda56d3610c89324ddbf4f1239
+size 105073710
